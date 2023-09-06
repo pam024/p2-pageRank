0912d2dc1a588a5eb52b2b49a48572ef0968ea31
pam024 <pmena0424@gmail.com>
2023-09-06 18:53:57 -0500

    readme & graph

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# p2-pageRank
+# PageRank
+
+## Problema:
+
+Un representante del departamento de marketing de una trasnacional desea incluir en su punto de
+análisis de la reunión, el nivel de importancia de 15 páginas Web dentro de una sección WWW
+seleccionada aleatoriamente. Para dar respuesta a esta necesidad, usted como ingeniero propone utilizar la técnica PageRank sobre la sección WWW seleccionada.
+
+## Requisitos:
+
+## Autores:
+
+\- [Joel del Castillo]()
+
+\- [Pamela Mena]()
+
+\- [Paúl Quimbita]()
+
+\- [José Santillán]()
---
+++ graph.py
@@ -0,0 +1,22 @@
+from manimlib.imports import *
+from manim import *
+import numpy as np
+
+
+class Graph(Scene):
+    def construct(self):
+        vertices = [1, 2, 3, 4]
+        edges = [(1, 2), (2, 3), (3, 4), (1, 3), (1, 4)]
+        g = Graph(vertices, edges)
+        self.play(Create(g))
+        self.wait()
+        self.play(g[1].animate.move_to([1, 1, 0]),
+                  g[2].animate.move_to([-1, 1, 0]),
+                  g[3].animate.move_to([1, -1, 0]),
+                  g[4].animate.move_to([-1, -1, 0]))
+        self.wait()
+    
+
+if __name__ == "__main__":
+    Graph()
+    
