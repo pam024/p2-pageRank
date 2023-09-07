be76b2bbe00849c8d7609840a2f228f6280af146
pam024 <pmena0424@gmail.com>
2023-09-06 20:46:00 -0500

    graph and matrix

--- graph.py
+++ graph.py
@@ -1,22 +1,39 @@
-from manimlib.imports import *
-from manim import *
-import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
 
-
-class Graph(Scene):
-    def construct(self):
-        vertices = [1, 2, 3, 4]
-        edges = [(1, 2), (2, 3), (3, 4), (1, 3), (1, 4)]
-        g = Graph(vertices, edges)
-        self.play(Create(g))
-        self.wait()
-        self.play(g[1].animate.move_to([1, 1, 0]),
-                  g[2].animate.move_to([-1, 1, 0]),
-                  g[3].animate.move_to([1, -1, 0]),
-                  g[4].animate.move_to([-1, -1, 0]))
-        self.wait()
-    
 
 if __name__ == "__main__":
-    Graph()
-    
+    G = nx.DiGraph()
+
+    nodes = ["A", "B", "C", "D", "E", "F", "G",
+             "H", "I", "J", "K", "L", "M", "N", "O"]
+    G.add_nodes_from(nodes)
+
+    edges_and_weights = [
+        ("A", "C", 1/3), ("A", "N", 1/3), ("A", "K", 1/3),
+        ("C", "D", 1/2), ("C", "K", 1/2),
+        ("E", "E", 1),
+        ("F", "M", 1/2), ("F", "G", 1/2),
+        ("G", "I", 1/2), ("G", "J", 1/2),
+        ("H", "G", 1/2), ("H", "F", 1/2),
+        ("I", "H", 1/2), ("I", "E", 1/2),
+        ("J", "I", 1/2), ("J", "L", 1/2),
+        ("K", "F", 1),
+        ("M", "B", 1/2), ("M", "A", 1/2),
+        ("N", "O", 1), ("O", "N", 1)
+    ]
+
+    for init_node, end_node, weight in edges_and_weights:
+        G.add_edge(init_node, end_node, weight=weight)
+
+    adyacent_matrix = nx.attr_matrix(G, edge_attr='weight', rc_order=nodes)
+
+    print("Matriz de adyacencia ORIGINAL")
+    print(adyacent_matrix)
+
+"""    pos = nx.spring_layout(G)  # Layout para la visualización`
+    nx.draw(G, pos, with_labels=True, node_size=800, node_color="skyblue",
+            font_size=12, font_color="black", font_weight="bold")
+    plt.title("Grafo")
+    plt.show()
+"""
